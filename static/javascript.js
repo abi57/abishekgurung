fbf08794cbde9242fbff93022f5e539a7a25850f
abishek <abhisekgrg05@gmail.com>
2022-02-25 00:56:29 +0545

    changes made

---
+++ static/javascript.js
@@ -0,0 +1,12 @@
+// let count = document.getElementById("displayscreen");
+// let btnInc = document.getElementById("btnInc");
+// let btnDec = document.getElementById("btnDec");
+// btnInc.addEventListener("click", () => {
+//   count.value = parseInt(count.value) + 1;
+// });
+// btnDec.addEventListener("click", () => {
+//   count.value = parseInt(count.value) - 1;
+// });
+//   <input type="text" value="0" id="displayscreen">
+//             <button id="btnInc" >Increment</button>
+//             <button id="btnDec" >Decrement</button>
