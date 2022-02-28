962b3d8e38ba51306b23595e6579dc50f51a7136
abishek <abhisekgrg05@gmail.com>
2022-02-28 15:16:33 +0545

    changesmade

--- static/javascript.js
+++ static/javascript.js
@@ -10,3 +10,28 @@
 //   <input type="text" value="0" id="displayscreen">
 //             <button id="btnInc" >Increment</button>
 //             <button id="btnDec" >Decrement</button>
+
+const showUp = () => {
+  document.getElementsByTagName("li")[0].style.left = "30px";
+  document.getElementsByTagName("li")[1].style.left = "30px";
+  document.getElementsByTagName("li")[2].style.left = "30px";
+  document.getElementsByTagName("li")[3].style.left = "30px";
+
+  document.getElementById("two").style.left = "0px";
+  document.getElementById("one").style.left = "500px";
+};
+
+const hide = () => {
+  document.getElementsByTagName("li")[0].style.left = "200px";
+  document.getElementsByTagName("li")[1].style.left = "200px";
+  document.getElementsByTagName("li")[2].style.left = "200px";
+  document.getElementsByTagName("li")[3].style.left = "200px";
+  document.getElementById("two").style.left = "100px";
+  document.getElementById("one").style.left = "0px";
+};
+
+if (hide) {
+  document.getElementById("one").style.zIndex = "1";
+} else {
+  document.getElementById("one").style.zIndex = "1";
+}
